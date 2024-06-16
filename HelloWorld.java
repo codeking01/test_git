32923354aaefc55673640edeee6e60eed4086301
codeking <xiongjialiang01@gmail.com>
2024-06-16 13:25:02 +0800

    dev的第3个方法,revise Helloword file, add new file

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * @author codeking01
+ * @date 2024/6/9
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String a=new String("Hello World");
+        System.out.println("Hello World!");
+    }
+
+    // 添加了一个新程序
+    public static void test() {
+        System.out.println("dev的第一个方法");
+    }
+
+    public static void test2() {
+        System.out.println("dev的第2个方法");
+    }
+
+    public static void test3() {
+        System.out.println("dev的第3个方法,测试一下功能");
+    }
+}
---
+++ gitAmendFile.java
@@ -0,0 +1,7 @@
+/**
+ * @author codeking01
+ * @date 2024/7/3
+ */
+
+public class gitAmendFile {
+}
